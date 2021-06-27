d910c05a885a328ccb98787200014f9e140392fb
ray <rayvanet@gmail.com>
2021-06-27 13:45:01 +0500

    all methods well tested internally, working

--- memock.js
+++ memock.js
@@ -7,21 +7,37 @@ const core = require('ray-core');
 const serve = require('ray-serve');
 const hash = require('ray-hash');
 const fetch = require('node-fetch');
+const zeros = require('ironberry').getLeadingZeros;
 
 const memockAuthors = "Ray Voice and Anna Voice";
-const server = fs.readJSON('basic-server-info.json');
+const baseServer = fs.readJSON('basic-server-info.json').value;
+baseServer.port = serve.port;
+baseServer.delayToMimicRealLatency = serve.latency;
 const memockLogs = "memock-logs.txt";
 
-logs.init(memockAuthors, server.version);
+logs.init(memockAuthors, baseServer.version);
 
 module.exports = {
+  server: baseServer,
   init: function(callback) {serve.listen(callback)},
-  usePort: (port) => { serve.port = port },
+  usePort: function(port) {serve.port = this.server.port = port},
   mainPageJSON: (sName, sVersion) => { serve.showRoot(sName, sVersion) },
-  setServerDelay: (time) => {serve.latency = time},
+  setServerDelay: function(time) {serve.latency = this.server.delayToMimicRealLatency = time},
   nodeNameOf: (fileIndex) => `file{+fileIndex+1`,
   bindJSONAtNode: function(node, json) { serve.serveJSON(node, json) },
-  bindFileAtNode: function(node, fileName, fileDir) { serve.serveFile(node, fileName, fileDir) },
-   
+  bindFileAtNode: function(node, file) {
+    const fileHash = hash
+		       .getHashOfFile(file)
+		       .value;
+    console.log(fileHash, this.server);
+    this.server
+	  .map
+	  .push({fileName: file, hash: fileHash});
+    serve.serveFile(node, file);
+  },
+  static: function(dir) {
+    serve.static(dir);
+  },
+  getShardPrefix: (totalFiles, fileNumber) => zeros(totalFiles, fileNumber)
 }
 
